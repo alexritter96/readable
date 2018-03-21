64a0a5e02cf867e42677642af290913891936cf8
Alex Ritter <alexanderritter@comcast.net>
2018-03-20 21:08:55 -0400

    first commit

---
+++ read.py
@@ -0,0 +1,85 @@
+""" Readable — a CLI tool for readability text analysis
+Usage:
+  read <text> [--flesch-kincaid]
+  read <text> [--gunning-fog]
+  read <text> [--smog]
+  read <text> [--automated]
+  read <text> [--chars]
+  read <text> [--sentences]
+  read <text> [--words]
+  read <text> [--syllables]
+  read <text> [--avgchars]
+  read <text> [--avgwords]
+  read <text> [--htmltext]
+  read (-h | --help)
+  read --version
+Options:
+  -h --help           Show this screen.
+  --version           Show version.
+  --flesch-kincaid    Returns the Flesch-Kincaid Grade Level
+  --gunning-fog       Returns the Gunning Fog Index
+  --smog              Returns the Smog Index
+  --automated         Returns the Automated Readability Index
+  --chars             Returns the Total Number of Characters
+  --sentences         Returns the Total Number of Sentences
+  --words             Returns the Total Number of Words
+  --syllables         Returns Average Number of Syllables per Word
+  --avgchars          Returns the Average Number of Characters per Word
+  --avgwords          Returns the Average Number of Words per Sentence
+  --htmltext          Returns Text of HTML Document
+
+"""
+
+from docopt import docopt
+from readlib import Readability
+import crayons
+
+
+def main():
+    args = docopt(__doc__, version='Readable v1.0')
+    text = "In fact, knowledge in the precise sense of the word is always knowledge of an object. Of a particular object, for it is impossible to know in general, without knowing a particular system of phenomena, whether these are chemical, psychological, or some other phenomena."
+    read = Readability(text)
+
+    if args['<text>']:
+        if args['--flesch-kincaid']:
+            print(crayons.red(read.flesch_kincaid()))
+
+        if args['--gunning-fog']:
+            print(crayons.blue(read.gunning_fog()))
+
+        if args['--smog']:
+            print(read.smog_index())
+
+        if args['--automated']:
+            print(read.ari())
+
+        if args['--chars']:
+            print("Characters: {}".format(read.char()))
+
+        if args['--sentences']:
+            print("Sentences: {}".format(read.sent_count()))
+
+        if args['--words']:
+            print("Words: {}".format(read.word_count()))
+
+        if args['--avgchars']:
+            print("Average Number of Characters Per Word: {}".format(read.avg_char()))
+
+        if args['--syllables']:
+            print("Syllables per word: {}".format(read.avg_syl()))
+
+        if args['--avgwords']:
+            print("Average Number of Words Per Sentence: {}".format(read.avg_words()))
+
+        if args['--htmltext']:
+            print(read.get_url('https://www.marxists.org/archive/marx/works/1844/manuscripts/hegel.htm#44H6'))
+
+
+if __name__ == '__main__':
+
+    main()
+
+
+
+
+
---
+++ readlib.py
@@ -0,0 +1,113 @@
+import math
+import re
+import requests
+
+from nltk.corpus import cmudict
+from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import sent_tokenize
+
+from bs4 import BeautifulSoup
+
+d = cmudict.dict()
+tokenizer = RegexpTokenizer(r'\w+')
+
+
+class Readability:
+
+    def __init__(self, txt):
+        self.sents = sent_tokenize(txt)
+        self.words = tokenizer.tokenize(txt)
+        self.txt = txt
+
+    def char(self):
+        char = 0
+        for word in self.words:
+            for w in word:
+                char += 1
+
+        return char
+
+    def sent_count(self):
+        return len(self.sents)
+
+    def word_count(self):
+        return len(self.words)
+
+    def syl(self, word):
+        # returns number of syllables per word
+        try:
+            syllable = [len(list(y for y in x if y[-1].isdigit()))
+                        for x in d[word.lower()]]
+            return syllable[0]
+
+        except KeyError:
+            return None
+
+    def list_to_word(self):
+        # tokenizes all words. For each token, the syl function is called.
+        # Returns the number of syllables for each token in a list.
+        len_syl = []
+        word = self.words
+
+        for w in word:
+            len_syl.append(self.syl(w))
+
+        return len_syl
+
+    def avg_syl(self):
+        return sum(self.list_to_word()) / self.word_count()
+
+    def avg_char(self):
+        return self.char() / self.word_count()
+
+    def avg_words(self):
+        return self.word_count() / self.sent_count()
+
+    def poly_syl(self):
+        poly_syl = []
+
+        for p in self.list_to_word():
+            if p >= 3:
+                poly_syl.append(p)
+
+        return poly_syl
+
+    def flesch_kincaid(self, ease=False):
+        # Flesch Kincaid algorithm determines the readability ease of a given text.
+        # Higher score indicates easier comprehension and lower score indicates more complexity
+        syl_int = sum(self.list_to_word())
+        TWS = self.word_count() / self.sent_count()
+        TSW = syl_int / self.word_count()
+
+        if ease:
+            return 206.835 - 1.015 * TWS - 84.6 * TSW
+
+        else:
+            x = 0.39 * TWS + 11.8 * TSW - 15.59
+            return "Flesch-Kincaid Grade Level: {}".format(x)
+
+    def gunning_fog(self):
+        counter = len(self.poly_syl())
+        TWS = self.word_count() / self.sent_count()
+        CWW = counter / self.word_count()
+        fog = 0.4 * (TWS + 100 * CWW)
+        return "Gunning Fog Index: {}".format(fog)
+
+    def smog_index(self):
+        # for accuracy, there must be at least 30 sentences
+        f = len(self.poly_syl()) * (30 / self.word_count())
+        smog = 1.0430 * math.sqrt(f) + 3.1291
+        return "Smog Index: {}".format(smog)
+
+    def ari(self):
+        chars = self.char()
+        f = 4.71 * (chars / self.word_count()) + 0.5 * (self.word_count() / self.sent_count()) - 21.43
+        return "Automated Readability Index: {}".format(f)
+
+    def get_url(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        return soup.get_text()
+
+
+
